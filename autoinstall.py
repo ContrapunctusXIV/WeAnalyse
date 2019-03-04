9044e2263a3d007fa0b9af157579516c6599f16a
ContrapunctusXIV <34133491+ContrapunctusXIV@users.noreply.github.com>
2019-03-04 17:29:35 +0800

    temporary fix

--- autoinstall.py
+++
@@ -1,9 +0,0 @@
-import os
-try:
- import numpy
- import matplotlib
- import pyecharts
- import pyecharts_snapshot
- import jieba
-except ImportError as e:
-    os.popen("pip install -r requirement.txt", "r")
--- run.bat
+++ run.bat
@@ -1,3 +1,14 @@
-python autoinstall.py
+python -m pip install --upgrade pip
+pip install --upgrade Cython
+pip install --upgrade wheel
+pip install --upgrade setuptools
+pip install numpy
+pip install jieba
+pip install lxml
+pip install matplotlib
+pip install pandas
+pip install pyecharts
+pip install PyMySQL
+
 python ./SimpleMode/autoRun.py
 pause
